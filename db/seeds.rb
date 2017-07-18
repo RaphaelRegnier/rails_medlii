00369907d5d773520e9e46e10d79ccde83e42caf
Raphael Regnier <raphael.regnier@hotmail.fr>
2017-07-18 17:36:37 -0400

    full seed with 542 instruments to choose from

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
-
 require "open-uri"
 
 PlayedInstrument.destroy_all
 User.destroy_all
 
+# COMMENT THESE LINES AFTER ONE SEED
+Instrument.destroy_all
 url = 'https://www.imit.org.uk/pages/a-to-z-of-musical-instrument.html'
 html_file = open(url)
 html_doc = Nokogiri::HTML(html_file)
@@ -12,22 +13,27 @@ instruments = []
 
 html_doc.search('table > tbody > tr > td:nth-child(1)').each do |element|
 
-instruments << element.text.match(/\A[a-zA-Z\s\/\'\"\’]*/)[0]
+  instruments << element.text.match(/\A[a-zA-Z\s\/\'\"\’]*/)[0]
 end
-
-profiles_pic = %w(http://lorempixel.com/400/400/people/1/ http://lorempixel.com/400/400/people/2/ http://lorempixel.com/400/400/people/3/ http://lorempixel.com/400/400/people/4/ http://lorempixel.com/400/400/people/5/ http://lorempixel.com/400/400/people/6/ http://lorempixel.com/400/400/people/7/ http://lorempixel.com/400/400/people/8/ http://lorempixel.com/400/400/people/9/ http://lorempixel.com/400/400/people/10/)
-instruments =
 
 instruments.each do |instrument|
-  Instrument.create(name: instrument)
+  Instrument.create(name: instrument.capitalize)
 end
+
+Instrument.find_by(name: "Instrument ").destroy
+Instrument.find_by(name: "T").destroy
+
+# < ------------------------------------------------------------------- UP TO HERE --->
+
+profiles_pic = %w(http://lorempixel.com/400/400/people/1/ http://lorempixel.com/400/400/people/2/ http://lorempixel.com/400/400/people/3/ http://lorempixel.com/400/400/people/4/ http://lorempixel.com/400/400/people/5/ http://lorempixel.com/400/400/people/6/ http://lorempixel.com/400/400/people/7/ http://lorempixel.com/400/400/people/8/ http://lorempixel.com/400/400/people/9/ http://lorempixel.com/400/400/people/10/)
+
 
 10.times do
   user = User.create(first_name: Faker::Name.first_name , last_name: Faker::Name.last_name,email: Faker::Internet.email, password: Faker::Internet.password)
 
   rand(1..5).times do
-    PlayedInstrument.create(level: rand(1..10), instrument_id: rand(1..120), user: user)
- end
+    PlayedInstrument.create(level: rand(1..10), instrument_id: rand(0..546), user: user)
+  end
 end
 
 
