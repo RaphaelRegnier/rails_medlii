a5f7d81feae7a9cc9930be3370607718ab1ad1b7
Hong Park <hongp26@gmail.com>
2017-07-21 17:01:08 -0400

    im not sure..

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :add_instruments, :edit, :destroy]
+  before_action :set_user, only: [:show, :add_instruments, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: [:new]
 
   def show
     birthday = @user.birth_date
@@ -7,14 +8,29 @@ class UsersController < ApplicationController
     @user_age = now.year - birthday.year - ((now.month > birthday.month || (now.month == birthday.month && now.day >= birthday.day)) ? 0 : 1)
   end
 
+  def new
+    @user = User.new
+  end
+
   def index
     @users = User.all
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to new_played_instrument_path
+    else
+      render :new
+    end
   end
 
   def edit
   end
 
   def update
+    @user.update(user_params)
+    redirect_to new_played_instrument_path
   end
 
   def destroy
@@ -23,14 +39,10 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :description, :photo)
+    params.require(:user).permit(:first_name, :last_name, :birth_date, :location, :email, :password, :description, :photo)
   end
 
   def set_user
     @user = User.find(params[:id])
-  end
-
-  def age(birthday)
-
   end
 end
