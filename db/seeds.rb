a124dcbbdd597a40e2dc87cd51b1e47191114a68
Raphael Regnier <raphael.regnier@hotmail.fr>
2017-07-25 14:44:46 -0400

    adding instruments

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,23 +2,23 @@ require "open-uri"
 puts "SEEDING..."
 PlayedInstrument.destroy_all
 User.destroy_all
+Instrument.destroy_all
 
 # COMMENT THESE LINES AFTER ONE SEED
-# Instrument.destroy_all
 # url = 'https://www.imit.org.uk/pages/a-to-z-of-musical-instrument.html'
 # html_file = open(url)
 # html_doc = Nokogiri::HTML(html_file)
-# puts "SEEDING INSTRUMENTS..."
-# instruments = []
+puts "SEEDING INSTRUMENTS..."
+instruments = ['electric guitar', 'keyboard', 'piano', 'drums', 'saxophone', 'bass guitar', 'acoustic guitar', 'classical guitar', 'violin', 'flute', 'trumpet', 'clarinet', 'cello', 'voice', 'synthetiser', 'ukulele', 'harp', 'xylophone', 'harmonica', 'viola', 'bass', 'bassoon', 'piccolo']
 
 # html_doc.search('table > tbody > tr > td:nth-child(1)').each do |element|
 
 #   instruments << element.text.match(/\A[a-zA-Z\s\'\"\’]*/)[0]
 # end
 
-# instruments.each do |instrument|
-#   Instrument.create(name: instrument.capitalize)
-# end
+instruments.each do |instrument|
+  Instrument.create(name: instrument.capitalize)
+end
 
 # Instrument.find_by(name: "Instrument ").destroy
 # Instrument.find_by(name: "T").destroy
