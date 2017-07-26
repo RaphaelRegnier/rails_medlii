e5f93bfd737cd23f17edbab394eb873b9d36c3e1
Hong Park <hongp26@gmail.com>
2017-07-26 17:47:25 -0400

    fix the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,7 +14,7 @@ end
 i = 0
 profiles_pic = %w(http://lorempixel.com/400/400/people/1/ http://lorempixel.com/400/400/people/2/ http://lorempixel.com/400/400/people/3/ http://lorempixel.com/400/400/people/4/ http://lorempixel.com/400/400/people/5/ http://lorempixel.com/400/400/people/6/ http://lorempixel.com/400/400/people/7/ http://lorempixel.com/400/400/people/8/ http://lorempixel.com/400/400/people/9/ http://lorempixel.com/400/400/people/10/)
 
-addresses = ["1234 Drummond Street, Montreal, QC, Canada", "1234 Stanley Street, Montreal, QC, Canada", "3879 Wellington Street, Verdun, QC, Canada", "3879 Ontario Street East, Montreal, QC, Canada", "4654 Rue Saint-Jacques, Montreal, QC, Canada", "4654 Rue Saint-Urbain, Montreal, QC, Canada", "999 Boulevard Saint-Laurent, Montreal, QC, Canada", "1112 Crolssant Verdon Laval, QC, Canada", "4430 Rue Saint-Antoine Ouest, Montreal, QC, Canada", "5930 Montreal Road Ottawa, ON, Canada"]
+addresses = ["1234 Drummond Street, Montreal, QC, Canada", "1000 de la commune, Montréal, QC, Canada", "3879 Wellington Street, Verdun, QC, Canada", "3879 Ontario Street East, Montreal, QC, Canada", "2300 Boul. du Tricentenaire, Montréal, QC, Canada", "4654 Rue Saint-Urbain, Montreal, QC, Canada", "4500 Ch. de la Côte-des-Neiges, Westmount, QC, Canada", "1112 Crolssant Verdon Laval, QC, Canada", "4430 Rue Saint-Antoine Ouest, Montreal, QC, Canada", "5930 Montreal Road Ottawa, ON, Canada"]
 
 10.times do
   user = User.new(first_name: Faker::Name.first_name , last_name: Faker::Name.last_name,email: Faker::Internet.email, password: Faker::Internet.password, description: Faker::Lorem.paragraph, birth_date: Faker::Date.birthday(18, 65), location: addresses[i], photo_url: profiles_pic[i])
