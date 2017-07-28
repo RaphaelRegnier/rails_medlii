e9e1354f68ada8ab1cdfcb21572ddef8c8adca2a
Cristina Llorente <aun.cris@gmail.com>
2017-07-27 23:25:16 -0400

    working with user 2 - showing name in index

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,42 +1,42 @@
 class AppointmentsController < ApplicationController
 
-def index
-  @appointments = Appointment.all
-  @conversation = Conversation.find(params[:conversation_id])
-end
+  def index
+    @appointments = Appointment.all
+  end
 
-def new
-  @appointment = Appointment.new
-  @conversation = Conversation.find(params[:conversation_id])
-end
+  def new
+    @appointment = Appointment.new
+    @conversation = Conversation.find(params[:conversation_id])
+  end
 
-def create
-  @appointment = Appointment.create(appointment_params)
-  @appointment.user_id = current_user.id
-  @conversation = Conversation.find(params[:conversation_id])
-  @appointment.conversation_id = @conversation.id
-  @appointment.save!
-  redirect_to conversation_appointment_path(@conversation, @appointment)
-  # conversation_path(@conversation)
+  def create
+    @conversation = Conversation.find(params[:conversation_id])
 
-end
+    @appointment = Appointment.create(appointment_params)
+    @appointment.user_id = current_user.id
+    @appointment.user_2_id = @conversation.user_2_id
+    @appointment.conversation_id = @conversation.id
 
-def show
-  # set_appointment
-  # @appointment = Appointment.new
-end
+    @appointment.save!
+    redirect_to conversation_appointments_path(@conversation, @appointment)
+  end
+
+  def show
+    # set_appointment
+    # @appointment = Appointment.new
+  end
 
 
-private
-def set_appointment
-  @appointment = Appointment.find(params[:id])
-end
+  private
+  def set_appointment
+    @appointment = Appointment.find(params[:id])
+  end
 
-def set_conversation
-  @conversation = Conversation.find(params[:conversation_id])
-end
+  def set_conversation
+    @conversation = Conversation.find(params[:conversation_id])
+  end
 
-def appointment_params
-  params.require(:appointment).permit(:address, :date)
-end
+  def appointment_params
+    params.require(:appointment).permit(:address, :date)
+  end
 end
--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,4 +1,8 @@
 class Appointment < ApplicationRecord
   belongs_to :user, :foreign_key => "user_id"
   belongs_to :conversation
+
+  def other_user
+    User.find(user_2_id)
+  end
 end
